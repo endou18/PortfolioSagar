6c667e8bcfc9ea7e7ca27ab662b5cd9c65a03548
endou18 <imsgr10@gmail.com>
2025-02-16 23:33:37 +0530

    MinorChange

--- app/components/about-section/About.tsx
+++ app/components/about-section/About.tsx
@@ -27,7 +27,7 @@ export default function About() {
         charSpace={"mr-[0.001em]"}
         className={`uppercase ${syne.className} antialiased text-4xl md:text-5xl xl:text-6xl font-bold opacity-80`}
       >
-        I BUILD SCALABLE & INNOVATIVE SOLUTIONS FOR THE WEB
+        I BUILD SCALABLE AND INNOVATIVE SOLUTIONS FOR THE WEB
       </AnimatedTitle>
 
       <div className="grid grid-cols-1 lg:grid-cols-[8.5fr_3.5fr] gap-8 mt-6">
